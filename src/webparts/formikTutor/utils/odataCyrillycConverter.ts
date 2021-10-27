febf8515233cf1b3e54ecaee560fba6f5de3e4e8
marach.dv <marach.dv@modul.tech>
2021-10-27 12:48:28 +0300

    debuged converters

--- src/webparts/formikTutor/utils/odataCyrillycConverter.ts
+++ src/webparts/formikTutor/utils/odataCyrillycConverter.ts
@@ -1,17 +1,26 @@
-export const convertCyrillicToOdata = (columnName: string) => {
-  let resultString: string = "";
+export const convertCyrillicToOdata = (columnName: string): string => {
+  let resultString = "";
   for (let i = 0; i < columnName.length; i++) {
-    let charCode = columnName.codePointAt(i).toString(16);
-    charCode = charCode.length != 4 ? "0" + charCode : charCode;
-    resultString += "_x" + charCode + "_";
+    if (columnName.charAt(i).match(/[А-Яа-я]/)) {
+      let charCode = columnName.codePointAt(i).toString(16);
+      charCode = charCode.length !== 4 ? "0" + charCode : charCode;
+      resultString += "_x" + charCode + "_";
+    } else {
+      resultString += columnName.charAt(i);
+    }
   }
   return resultString;
 };
 
-export const convertOdataToCyrillic = (encodedColumnName: string) => {
-  const encodedCharacters: number[] = encodedColumnName
+const convertOdataToCyrillic = (encodedColumnName: string) => {
+  const decodedCharacters = encodedColumnName
     .split("_")
     .filter(String)
-    .map((encodedCharacter) => parseInt(encodedCharacter.slice(1), 16));
-  return String.fromCodePoint(...encodedCharacters);
+    .map((character: string) => {
+      if (!character.match(/x\d+/)) return character;
+      return String.fromCodePoint(parseInt(character.slice(1), 16));
+    });
+  return decodedCharacters.reduce(
+    (acc: string, character: string) => acc + character
+  );
 };
